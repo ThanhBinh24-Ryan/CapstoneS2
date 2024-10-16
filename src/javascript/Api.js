166571f1e2beb31b021e5131bfbd2975cc9f4286
Binh <thanhbinh12b4lh2022@gmail.com>
2024-10-17 00:57:36 +0700

    add api

--- src/javascript/Api.js
+++ src/javascript/Api.js
@@ -5,5 +5,34 @@ class Api{
           method: "GET",
         });
       }
+      deletePhoneById(id) {
+        return axios({
+          url: `https://670fd53ea85f4164ef2c1d34.mockapi.io/capstone2/${id}`,
+          method: "DELETE",
+        });
+      }
+    
+      addPhone(capstone2) {
+        return axios({
+          url: `https://670fd53ea85f4164ef2c1d34.mockapi.io/capstone2`,
+          method: "POST",
+          data: product,
+        });
+      }
+    
+      getPhoneById(id) {
+        return axios({
+          url: `https://670fd53ea85f4164ef2c1d34.mockapi.io/capstone2/${id}`,
+          method: "GET",
+        });
+      }
+    
+      updatePhoneById(product) {
+        return axios({
+          url: `https://670fd53ea85f4164ef2c1d34.mockapi.io/capstone2/${capstone2.id}`,
+          method: "PUT",
+          data: product,
+        });
+      }
 }
 export default Api;
