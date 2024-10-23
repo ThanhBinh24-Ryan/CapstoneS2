8f818dc6c32b488384012184f25f431da616601f
Binh <thanhbinh12b4lh2022@gmail.com>
2024-10-23 17:09:04 +0700

    new admin

--- src/javascript/Api.js
+++ src/javascript/Api.js
@@ -16,7 +16,7 @@ class Api{
         return axios({
           url: `https://670fd53ea85f4164ef2c1d34.mockapi.io/capstone2`,
           method: "POST",
-          data: product,
+          data: capstone2,
         });
       }
     
@@ -27,12 +27,13 @@ class Api{
         });
       }
     
-      updatePhoneById(product) {
+      updatePhoneById(capstone2) {
         return axios({
           url: `https://670fd53ea85f4164ef2c1d34.mockapi.io/capstone2/${capstone2.id}`,
           method: "PUT",
-          data: product,
-        });
+          data: capstone2,
+        });z
       }
+      
 }
 export default Api;
